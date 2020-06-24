3b36d0b8bbe5199bcc3cf46f2cb9bde325485248
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2020-06-24 19:53:01 +0200

    log utils

---
+++ system9/__init__.py
@@ -0,0 +1,10 @@
+import logging
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(name)-12s %(levelname)-8s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+logging.getLogger("opentrons").setLevel(logging.WARNING)
+logging.getLogger("asyncio").setLevel(logging.WARNING)
