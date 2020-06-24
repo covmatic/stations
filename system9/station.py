ee63f3c76fd3fe61989b0eb7cd2a1d0af08f0fe9
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2020-06-24 20:02:27 +0200

    abstract station

---
+++ system9/station.py
@@ -0,0 +1,62 @@
+from .utils import ProtocolContextLoggingHandler, logging
+from functools import wraps
+from typing import Optional, Callable, Any
+
+
+def loader(key):
+    def loader_(idx: int = 0, *items: tuple) -> Callable:
+        def _labware_loader(method: Callable) -> Callable:
+            @wraps(method)
+            def method_(self, *args, **kwargs):
+                if items:
+                    self.logger.debug("loading {}".format(", ".join(map(str, items))))
+                return method(self, *args, **kwargs)
+            
+            setattr(method_, key, (idx, items))
+            return method_
+        return _labware_loader
+    return loader_
+
+
+# Decorator for labware loader methods
+labware_loader = loader("_labware_load")
+# Decorator for instrument loader methods
+instrument_loader = loader("_instr_load")
+
+
+class Station:
+    def __getattr__(self, item: str):
+        return self.metadata[item]
+    
+    @property
+    def logger(self) -> logging.getLoggerClass():
+        if ((not hasattr(self, "_logger")) or self._logger is None) and self._ctx is not None:
+            self._logger = logging.getLogger(self.loggerName)
+            self._logger.addHandler(ProtocolContextLoggingHandler(self._ctx))
+        return self._logger
+    
+    @property
+    def loggerName(self) -> str:
+        return self.__class__.__name__
+    
+    @classmethod
+    def loaders(cls, key: str) -> map:
+        return map(lambda x: (x[0], x[2]), sorted(map(lambda x: (x, *getattr(getattr(cls, x), key)), filter(lambda x: hasattr(getattr(cls, x), key), dir(cls))), key=lambda x: x[1]))
+    
+    @classmethod
+    def labwareLoaders(cls) -> map:
+        return cls.loaders("_labware_load")
+    
+    @classmethod
+    def instrumentLoaders(cls) -> map:
+        return cls.loaders("_instr_load")
+    
+    def loadIt(self, it):
+        for method_name, _ in it:
+            getattr(self, method_name)()
+    
+    def loadLabware(self):
+        self.loadIt(self.labwareLoaders())
+    
+    def loadInstruments(self):
+        self.loadIt(self.instrumentLoaders())
