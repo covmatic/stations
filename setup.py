6a699dacc8ecc7bf1ed6a480d9adb755980775a6
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2020-07-02 09:31:06 +0200

    packaging vars

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
 import setuptools
+import codecs
+import os.path
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
 setuptools.setup(
     name="covid19-system9",
-    version="0.0.0.dev",
+    version=get_version("system9/__init__.py"),
     author="Marco Tiraboschi",
     author_email="marcotiraboschi@hotmail.it",
     description="Package for the COVID19 System-9 (Calcinate)",
