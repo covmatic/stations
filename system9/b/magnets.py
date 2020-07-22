5147bc5955920e8487e7d69a4eab909d02d9aa86
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2020-07-22 14:56:06 +0200

    env variable

--- system9/b/magnets.py
+++ system9/b/magnets.py
@@ -3,13 +3,14 @@ import os
 from collections import namedtuple
 
 
+_env_key = "OT_MAGNET_JSON"
 _keys = ["serial", "station", "height"]
 _getter_c = namedtuple("getter", list(map("by_{}".format, _keys)))
 
 
 def __getattr__(name: str):
     if name == "specs":
-        fp = os.path.join(os.path.split(__file__)[0], "magnet_heights.json")
+        fp = os.environ.get(_env_key, os.path.join(os.path.split(__file__)[0], "magnet_heights.json"))
         with open(fp, "r") as f:
             j = json.load(f)
         return j
