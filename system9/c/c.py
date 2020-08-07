3aba8ac22bc4f99a9975b7a7e287f92996f6d0b6
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2020-08-07 10:04:14 +0200

    more parameters in station C

--- system9/c/c.py
+++ system9/c/c.py
@@ -16,13 +16,21 @@ class StationC(Station):
         drop_threshold: int = 296,
         jupyter: bool = True,
         logger: Optional[logging.getLoggerClass()] = None,
+        mastermix_vol: float = 12,
         mastermix_vol_headroom: float = 1.1,
         metadata: Optional[dict] = None,
         num_samples: int = 96,
         positive_control_well: str = 'A10',
+        sample_blow_height: float = -2,
+        sample_bottom_height: float = 2,
+        sample_mix_vol: float = 10,
+        sample_mix_reps: int = 1,
+        sample_vol: float = 8,
         samples_per_col: int = 8,
         samples_per_cycle: int = 96,
         skip_delay: bool = False,
+        suck_height: float = 2,
+        suck_vol: float = 5,
         tipracks_slots: Tuple[str, ...] = ('2', '3', '6', '7', '9'),
         tip_log_filename: str = 'tip_log.json',
         tip_log_folder_path: str = './data/C',
@@ -34,13 +42,21 @@ class StationC(Station):
         :param drop_loc_r: offset for dropping to the right side (should be negative) in mm
         :param drop_threshold: the amount of dropped tips after which the run is paused for emptying the trash
         :param logger: logger object. If not specified, the default logger is used that logs through the ProtocolContext comment method
+        :param mastermix_vol: Mastermix volume per sample in uL
         :param mastermix_vol_headroom: Headroom for mastermix volume as a multiplier
         :param metadata: protocol metadata
         :param num_samples: The number of samples that will be loaded on the station B
         :param positive_control_well: Position of the positive control well
+        :param sample_blow_height: Height from the top when blowing out in mm (should be negative)
+        :param sample_bottom_height: Height to keep from the bottom in mm when dealing with samples
+        :param sample_mix_vol: Samples mixing volume in uL
+        :param sample_mix_reps: Samples mixing repetitions 
+        :param sample_vol: Sample volume
         :param samples_per_col: The number of samples in a column of the destination plate
         :param samples_per_cycle: The number of samples processable in one cycle
         :param skip_delay: If True, pause instead of delay.
+        :param suck_height: Height from the top when sucking in any remaining droplets on way to trash in mm
+        :param suck_vol: Volume for sucking in any remaining droplets on way to trash in uL
         :param tip_log_filename: file name for the tip log JSON dump
         :param tip_log_folder_path: folder for the tip log JSON dump
         :param tip_track: If True, try and load previous tiprack log from the JSON file
@@ -60,9 +76,17 @@ class StationC(Station):
             tip_track=tip_track,
         )
         self._bottom_headroom_height = bottom_headroom_height
+        self._mastermix_vol = mastermix_vol
         self._mastermix_vol_headroom = mastermix_vol_headroom
-        self._positive_control_well = positive_control_well 
+        self._positive_control_well = positive_control_well
+        self._sample_blow_height = sample_blow_height
+        self._sample_bottom_height = sample_bottom_height
+        self._sample_mix_vol = sample_mix_vol
+        self._sample_mix_reps = sample_mix_reps
+        self._sample_vol = sample_vol
         self._samples_per_cycle = samples_per_cycle
+        self._suck_height = suck_height
+        self._suck_vol = suck_vol
         self._tipracks_slots = tipracks_slots
         
         self._remaining_samples = self._num_samples
@@ -137,13 +161,6 @@ class StationC(Station):
         return self._tube_block.wells()[0]
     
     @property
-    def mm_dict(self):
-        return {
-            'volume': 12,
-            'components': {tube: vol for tube, vol in zip(self._tube_block.columns()[1], [10, 2])}
-        }
-    
-    @property
     def mm_strip(self):
         return self._mm_strips.columns()[0]
     
@@ -152,7 +169,7 @@ class StationC(Station):
         return int(math.ceil(min(self._remaining_samples, self._samples_per_cycle) / self._m20.channels))
     
     def fill_mm_strips(self):
-        vol_per_strip_well = self.remaining_cols * self.mm_dict['volume'] * self._mastermix_vol_headroom
+        vol_per_strip_well = self.remaining_cols * self._mastermix_vol * self._mastermix_vol_headroom
         
         self.pick_up(self._p300)        
         for well in self.mm_strip:
@@ -161,18 +178,17 @@ class StationC(Station):
         self._p300.drop_tip()
     
     def transfer_mm(self):
-        mm_vol = self.mm_dict['volume']
         self.pick_up(self._m20)
-        self._m20.transfer(mm_vol, self.mm_strip[0].bottom(self._bottom_headroom_height), self.sample_dests[:self.remaining_cols], new_tip='never')
+        self._m20.transfer(self._mastermix_vol, self.mm_strip[0].bottom(self._bottom_headroom_height), self.sample_dests[:self.remaining_cols], new_tip='never')
         self._m20.drop_tip()
     
     def transfer_sample(self, vol: float, source, dest):
         self.logger.debug("transferring {:.0f} uL from {} to {}".format(vol, source, dest))
         self.pick_up(self._m20, tiprack="_tips20_no_a" if source.display_name.split(" ")[0] == self._positive_control_well else None)
-        self._m20.transfer(vol, source.bottom(2), dest.bottom(2), new_tip='never')
-        self._m20.mix(1, 10, dest.bottom(2))
-        self._m20.blow_out(dest.top(-2))
-        self._m20.aspirate(5, dest.top(2))
+        self._m20.transfer(vol, source.bottom(self._sample_bottom_height), dest.bottom(self._sample_bottom_height), new_tip='never')
+        self._m20.mix(self._sample_mix_reps, self._sample_mix_vol, dest.bottom(self._sample_bottom_height))
+        self._m20.blow_out(dest.top(self._sample_blow_height))
+        self._m20.aspirate(self._suck_vol, dest.top(self._suck_height))  # suck in any remaining droplets on way to trash
         self._m20.drop_tip()
         self._remaining_samples -= self._m20.channels
     
@@ -180,7 +196,7 @@ class StationC(Station):
         self.fill_mm_strips()
         self.transfer_mm()
         for s, d in zip(self.sources, self.sample_dests):
-            self.transfer_sample(20 - self.mm_dict['volume'], s, d)
+            self.transfer_sample(self._sample_vol, s, d)
             if self._remaining_samples <= 0:
                 break
     
