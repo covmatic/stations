a0bae97a07559dc27012e4d8abbd36014e936983
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2020-08-12 16:49:17 +0200

    Refactored request API and added config

--- system9/request.py
+++ system9/request.py
@@ -1,23 +1,26 @@
-from opentrons.protocol_api import ProtocolContext 
+from opentrons.protocol_api import ProtocolContext
+from typing import Callable, Optional
 from threading import Thread
 import cherrypy
 
 
-metadata = {'apiLevel': "2.5"}
+class ContextAPI(type):
+    @staticmethod
+    def context_api(method: str) -> Callable:
+        return cherrypy.expose(lambda self: getattr(self._ctx, method)())
+    
+    def __new__(mcs, classname: str, supers: tuple, classdict: dict):
+        classdict.update(zip(*(lambda a: (a, map(mcs.context_api, a)))(classdict.pop("_actions", []))))
+        return super(ContextAPI, mcs).__new__(mcs, classname, supers, classdict)
 
 
-class StationRESTServer:
-    def __init__(self, ctx: ProtocolContext):
+class StationRESTServer(metaclass=ContextAPI):
+    _actions = ["pause", "resume"]
+    
+    def __init__(self, ctx: ProtocolContext, config: Optional[dict] = None):
         super(StationRESTServer, self).__init__()
         self._ctx = ctx
-    
-    @cherrypy.expose
-    def resume(self):
-        self._ctx.resume()
-    
-    @cherrypy.expose
-    def pause(self):
-        self._ctx.pause()
+        self._config = config
     
     @staticmethod
     def stop():
@@ -26,15 +29,8 @@ class StationRESTServer:
 
 class StationRESTServerThread(StationRESTServer, Thread):
     def run(self):
-        cherrypy.quickstart(self)
+        cherrypy.quickstart(self, config=self._config)
     
     def join(self, timeout=None):
         self.stop()
         super(StationRESTServerThread, self).join()
-
-
-def run(ctx: ProtocolContext):
-    t = StationRESTServerThread(ctx)
-    t.start()
-    input("Press enter to stop")
-    t.join()
