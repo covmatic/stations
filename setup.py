9648019b92369730b41d8fe309502eef3a0e91b6
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2020-10-07 12:51:01 +0200

    Update licensing in pypi package

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setuptools.setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7, <3.9',
