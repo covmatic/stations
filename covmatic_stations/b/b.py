2dcce96f8d669480706e114d721143eb19c1fc11
agostinofacotti <66720600+agostinofacotti@users.noreply.github.com>
2021-02-13 21:00:18 +0100

    Added log with Magnetic Deck serial number and height

--- covmatic_stations/b/b.py
+++ covmatic_stations/b/b.py
@@ -230,7 +230,9 @@ class StationB(Station):
         self._magdeck = self._ctx.load_module('Magnetic Module Gen2', self._magdeck_slot)
         self._magdeck.disengage()
         if (self._magheight_load):
-            self._magheight = magnets.height.by_serial.get(self._magdeck._module._driver.get_device_info()['serial'], self._magheight)
+            mag_deck_serial = self._magdeck._module._driver.get_device_info()['serial']
+            self._magheight = magnets.height.by_serial.get(mag_deck_serial, self._magheight)
+            self.logger.info("Found MagDeck with serial {}. Engage height is {}mm".format(mag_deck_serial, self._magheight))
     
     @labware_loader(3, "_magplate")
     def load_magplate(self):
