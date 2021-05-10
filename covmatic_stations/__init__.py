797d2bbc4f33aefc4708b4cf448f990e16da3b9b
gfacoetti <67072772+gfacoetti@users.noreply.github.com>
2021-05-10 12:07:11 +0200

    Add files via upload

--- covmatic_stations/__init__.py
+++ covmatic_stations/__init__.py
@@ -1,16 +1,3 @@
-import logging
-
-__version__ = "2.2.1"
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s %(name)-12s %(levelname)-8s: %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
-)
-logging.getLogger("asyncio").setLevel(logging.WARNING)
-logging.getLogger("urllib3").setLevel(logging.WARNING)
-
-
 # Copyright (c) 2020 Covmatic.
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
