a0f2ea0fe032a5b08ffc24eb832c6eb038cb020b
Agostino Facotti <agostino.facotti@gmail.com>
2021-11-08 19:39:24 +0100

    bioer preparation: added samples transfer and flow rates

--- covmatic_stations/bioer/Bioer_preparation.py
+++ covmatic_stations/bioer/Bioer_preparation.py
@@ -1,19 +1,30 @@
 from ..station import Station, labware_loader, instrument_loader
 from ..multi_tube_source import MultiTubeSource
 from ..utils import uniform_divide, get_labware_json_from_filename, MoveWithSpeed
+from ..a.copan_48 import copan_48_corrected_specs
 
 
 class BioerPreparation(Station):
     def __init__(self,
                  num_samples: int = 16,
-                 pk_tube_bottom_height: float = 2,
                  deepwell_slot: int = 3,
+                 pk_tube_bottom_height: float = 2,
                  pk_dw_bottom_height: float = 13.5,
+                 pk_rate_aspirate: float = 100,
+                 pk_rate_dispense: float = 100,
                  pk_volume: float = 10,
                  pk_dead_volume: float = 5,
                  pk_vertical_speed: float = 20,
                  sample_vertical_speed: float = 20,
+                 sample_air_gap: float = 25,
+                 sample_rate_aspirate: float = 200,
+                 sample_rate_dispense: float = 200,
+                 sample_volume: float = 200,
+                 sample_destination_bottom_height: float = 13.5,
+                 source_racks_slots=[1],
+                 source_headroom_height: float = 6,
                  tube_block_model: str = 'opentrons_24_tuberack_nest_1.5ml_screwcap',
+                 tube_rack_slot: int = 6,
                  p300_max_volume: float = 200,
                  ** kwargs):
 
@@ -23,29 +34,50 @@ class BioerPreparation(Station):
             num_samples=num_samples,
             ** kwargs)
         self._pk_volume = pk_volume
+        self._pk_rate_aspirate = pk_rate_aspirate
+        self._pk_rate_dispense = pk_rate_dispense
         self._pk_vertical_speed = pk_vertical_speed
         self._sample_vertical_speed = sample_vertical_speed
         self._pk_tube_bottom_height = pk_tube_bottom_height
         self._pk_dw_bottom_height = pk_dw_bottom_height
         self._pk_dead_volume = pk_dead_volume
         self._deepwell_slot = deepwell_slot
+        self._sample_air_gap = sample_air_gap
+        self._sample_rate_aspirate = sample_rate_aspirate
+        self._sample_rate_dispense = sample_rate_dispense
+        self._sample_volume = sample_volume
+        self._sample_destination_bottom_height = sample_destination_bottom_height
+        self._source_racks_slots = source_racks_slots
+        self._source_headroom_height = source_headroom_height
         self._tube_block_model = tube_block_model
+        self._tube_rack_slot = tube_rack_slot
         self._p300_max_volume = p300_max_volume
 
-    @labware_loader(1, "_tips200")
+    @labware_loader(0, "_tips200")
     def load_tips200(self):
         self._tips200 = [self._ctx.load_labware('opentrons_96_filtertiprack_200ul', slot, '200ul filter tiprack')
                                 for slot in ['11']]
 
-    @labware_loader(1, "_tips200")
+    @labware_loader(1, "_tips1000")
     def load_tips1000(self):
         self._tips1000 = [self._ctx.load_labware('opentrons_96_filtertiprack_1000ul', slot, '1000ul filter tiprack')
                                 for slot in ['10']]
+
+    @labware_loader(2, "_source_racks")
+    def load_source_racks(self):
+        labware_def = copan_48_corrected_specs.labware_definition()
+        self._source_racks = [
+            self._ctx.load_labware_from_definition(
+                labware_def, slot,
+                'source tuberack ' + str(i + 1)
+            ) for i, slot in enumerate(self._source_racks_slots)
+        ]
+
     @labware_loader(4, "_tube_rack")
     def load_tube_rack(self):
-        self._tube_rack = self._ctx.load_labware(self._tube_block_model, '6', 'tube rack for proteinase')
+        self._tube_rack = self._ctx.load_labware(self._tube_block_model, self._tube_rack_slot, 'tube rack for proteinase')
 
-    @labware_loader(5, "_dests_plate")
+    @labware_loader(5, "_dests_plates")
     def load_dests_plate(self):
         self._dests_plates = [self._ctx.load_labware_from_definition(get_labware_json_from_filename("bioer_96_wellplate_2000ul.json"),
                                          slot,
@@ -72,6 +104,10 @@ class BioerPreparation(Station):
         return [sample for row in self.samples_destination_rows for sample in row]
 
     @property
+    def samples_source_single(self):
+        return [sample for rack in self._source_racks for sample in rack.wells()]
+
+    @property
     def proteinase_destinations(self):
         return self.samples_destination_single
 
@@ -80,6 +116,9 @@ class BioerPreparation(Station):
         return self._p300_max_volume - self._pk_dead_volume
 
     def transfer_proteinase(self):
+        self._p300.flow_rate.aspirate = self._pk_rate_aspirate
+        self._p300.flow_rate.dispense = self._pk_rate_dispense
+
         num_samples_per_aspirate = self.p300_available_vol // self._pk_volume
         aspirate_volume = num_samples_per_aspirate*self._pk_volume + self._pk_dead_volume
         self.log("One PK aspirate of {}ul can dispense to {} destination".format(aspirate_volume, num_samples_per_aspirate))
@@ -112,7 +151,36 @@ class BioerPreparation(Station):
                                speed=self._pk_vertical_speed, move_close=False):
                 self._p300.dispense(self._pk_volume)
 
+        self._p300.blow_out(self._tube_rack.wells()[0].top())
         self.drop(self._p300)
+
+    def transfer_sample(self, source, dest):
+        self.logger.debug("transferring from {} to {}".format(source, dest))
+        dest_dispense_air_gap_point = dest.bottom(self._sample_destination_bottom_height + 5)
+
+        self.pick_up(self._p1000)
+        with MoveWithSpeed(self._p1000,
+                           from_point=source.bottom(self._source_headroom_height + 30),
+                           to_point=source.bottom(self._source_headroom_height),
+                           speed=self._sample_vertical_speed, move_close=False):
+            self._p1000.aspirate(self._sample_volume)
+        self._p1000.air_gap(self._sample_air_gap)
+
+        self._p1000.dispense(self._sample_air_gap, dest_dispense_air_gap_point)
+        with MoveWithSpeed(self._p1000,
+                           from_point=dest_dispense_air_gap_point,
+                           to_point=dest.bottom(self._sample_destination_bottom_height),
+                           speed=self._sample_vertical_speed, move_close=False):
+            self._p1000.dispense(self._sample_volume)
+        self._p1000.air_gap(self._sample_air_gap)
+        self.drop(self._p1000)
+
+    def transfer_samples(self):
+        self._p1000.flow_rate.aspirate = self._sample_rate_aspirate
+        self._p1000.flow_rate.dispense = self._sample_rate_dispense
+
+        for s, d in zip(self.samples_source_single, self.samples_destination_single):
+            self.transfer_sample(s, d)
 
     def log(self, s):
         self._ctx.comment(s)
@@ -177,6 +245,7 @@ class BioerPreparation(Station):
         #     self.pause(pk_requirements, home=False)
         self.stage = "Transfer proteinase"
         self.transfer_proteinase()
+        self.transfer_samples()
         #
         # #mix_beads
         # if self._mix_beads_phase:
