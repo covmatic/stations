118e2fb980e3222c39bc3d90b905475f1877ba5e
agostino.facotti <agostino.facotti@gmail.com>
2022-04-28 16:33:20 +0200

    station A technogenetics saliva: lower sample aspiration height

--- covmatic_stations/a/technogenetics.py
+++ covmatic_stations/a/technogenetics.py
@@ -309,7 +309,7 @@ class StationATechnogenetics48Saliva(StationATechnogenetics48):
             sample_lateral_top_height=3,
             sample_lateral_x_move=-10,
             sample_lateral_y_move=0,
-            source_headroom_height=4,
+            source_headroom_height=3,
             *args,
             **kwargs
     ):
