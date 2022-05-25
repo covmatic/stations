cbd72c2a82188d144c40b7d0d399fc6a0beee0c2
agostino.facotti <agostino.facotti@gmail.com>
2022-05-25 13:13:07 +0200

    station A technogenetics saliva: increased mix rate

--- covmatic_stations/a/technogenetics.py
+++ covmatic_stations/a/technogenetics.py
@@ -303,9 +303,10 @@ class StationATechnogenetics48Saliva(StationATechnogenetics48):
     def __init__(
             self,
             lys_mix_repeats: int = 3,
+            lys_mix_volume=500,
+            lysis_mix_rate=800,
             lys_mix_last_rate=100,
             lys_mix_last_volume=250,
-            lys_mix_volume=500,
             sample_lateral_air_gap=25,
             sample_lateral_top_height=3,
             sample_lateral_x_move=-10,
@@ -316,9 +317,10 @@ class StationATechnogenetics48Saliva(StationATechnogenetics48):
     ):
         super(StationATechnogenetics48Saliva, self).__init__(
             lys_mix_repeats=lys_mix_repeats,
+            lys_mix_volume=lys_mix_volume,
+            lysis_rate_mix=lysis_mix_rate,
             lys_mix_last_rate=lys_mix_last_rate,
             lys_mix_last_volume=lys_mix_last_volume,
-            lys_mix_volume=lys_mix_volume,
             sample_lateral_air_gap=sample_lateral_air_gap,
             sample_lateral_top_height=sample_lateral_top_height,
             sample_lateral_x_move=sample_lateral_x_move,
